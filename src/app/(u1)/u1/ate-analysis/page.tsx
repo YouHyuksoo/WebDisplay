5cb8790cff437e79b62b933f12334c9aa0948569
YouHyuksoo <hsyou@jisheng.co.kr>
2026-03-30 22:14:59 -0700

    fix(u1-ate): rename hourly chart title to 시간대별 검사수량 / 합격률

--- src/app/(u1)/u1/ate-analysis/page.tsx
+++ src/app/(u1)/u1/ate-analysis/page.tsx
@@ -118,7 +118,7 @@ export default function AteAnalysisPage() {
             <ChartCard title="전일 vs 당일 합격률 비교" badge="전일/당일">
               {daily ? <AteYesterdayCompare lineStats={daily.lineStats} /> : <ChartSkeleton />}
             </ChartCard>
-            <ChartCard title="시간대별 검사 추이" badge="당일">
+            <ChartCard title="시간대별 검사수량 / 합격률" badge="당일">
               {daily ? <AteHourlyTrend hourlyTrend={daily.hourlyTrend} /> : <ChartSkeleton />}
             </ChartCard>
             <ChartCard title="주간 일별 합격률 추이" badge="주간">
